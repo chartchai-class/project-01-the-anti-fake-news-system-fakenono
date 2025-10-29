21180c635c83a19617f9c6950a03d33794055fe6
Phone <hponemyut@gmail.com>
2025-10-29 23:16:34 +0700

    Added Scroll Up Buttom

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,7 @@ function handleClickOutside(event) {
 const authUserRef = storeToRefs(authStore)
 onMounted(() => {
   document.addEventListener('click', handleClickOutside)
+  window.addEventListener('scroll', onScroll)
   watch(
     authUserRef.user,
     () => {
@@ -57,6 +58,7 @@ onMounted(() => {
 
 onBeforeUnmount(() => {
   document.removeEventListener('click', handleClickOutside)
+  window.removeEventListener('scroll', onScroll)
 })
 if (token && user) {
   authStore.reload(token, JSON.parse(user))
@@ -69,9 +71,33 @@ function logout() {
   authStore.logout()
   isOpen.value = false
 }
+
+const showScrollUp = ref<boolean>(false)
+function handleScrollUp() {
+  scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
+}
+function onScroll() {
+  showScrollUp.value = window.scrollY > 100
+}
 </script>
 
 <template>
+  <button
+    class="fixed md:right-4 right-1 bottom-4 transition"
+    @click.prevent="handleScrollUp"
+    v-show="showScrollUp"
+  >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+      <path
+        fill-rule="evenodd"
+        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm.53 5.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l1.72-1.72v5.69a.75.75 0 0 0 1.5 0v-5.69l1.72 1.72a.75.75 0 1 0 1.06-1.06l-3-3Z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  </button>
   <header class="bg-black sticky top-0 flex items-center justify-between px-6 pr-2 py-4 z-50">
     <!-- <div class="w-1/3"></div> -->
     <RouterLink to="/" class="text-4xl font-bold text-white text-center w-auto mr-auto"
